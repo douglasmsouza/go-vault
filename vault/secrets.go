310c6a526c48b0eac03d8d3637aa214dcb705a50
douglas-souza <douglas.souza@indyxa.com.br>
2022-03-24 10:04:53 -0300

    Adicionado suporte a fallback com variaveis de ambiente

--- vault/secrets.go
+++ vault/secrets.go
@@ -2,13 +2,15 @@ package vault
 
 import (
 	"encoding/json"
+	"os"
+	"strconv"
 )
 
 type Secret struct {
 	data map[string]interface{}
 }
 
-func (s Secret) GetString(name string, defaultVal string) string {
+func (s Secret) GetString(name, defaultVal string) string {
 	value := s.data[name]
 	if value == nil {
 		return defaultVal
@@ -40,6 +42,73 @@ func (s Secret) GetFloat(name string, defaultVal float64) (float64, error) {
 	value := s.data[name]
 	if value == nil {
 		return defaultVal, nil
+	}
+	return value.(json.Number).Float64()
+}
+
+func (s Secret) GetStringOrEnv(name, env, defaultVal string) string {
+	value := s.data[name]
+	if value == nil {
+		value = os.Getenv(env)
+		if value == "" {
+			return defaultVal
+		}
+	}
+	return value.(string)
+}
+
+func (s Secret) GetBoolOrEnv(name, env string, defaultVal bool) bool {
+	value := s.data[name]
+	if value == nil {
+		value = os.Getenv(env)
+		if value == "" {
+			return defaultVal
+		}
+
+		b, _ := strconv.ParseBool(value.(string))
+		return b
+	}
+	return value.(bool)
+}
+
+func (s Secret) GetIntOrEnv(name, env string, defaultVal int) (int, error) {
+	value := s.data[name]
+	if value == nil {
+		value = os.Getenv(env)
+		if value == "" {
+			return defaultVal, nil
+		}
+
+		i, err := strconv.Atoi(value.(string))
+		if err != nil {
+			return 0, err
+		}
+
+		return i, nil
+	}
+
+	i, err := value.(json.Number).Int64()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(i), nil
+}
+
+func (s Secret) GetFloatOrEnv(name, env string, defaultVal float64) (float64, error) {
+	value := s.data[name]
+	if value == nil {
+		value = os.Getenv(env)
+		if value == "" {
+			return defaultVal, nil
+		}
+
+		i, err := strconv.ParseFloat(value.(string), 64)
+		if err != nil {
+			return 0, err
+		}
+
+		return i, nil
 	}
 	return value.(json.Number).Float64()
 }
